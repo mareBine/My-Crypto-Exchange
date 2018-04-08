e26d9f1540c3b7edf050a0817125c3cd6821679f
Marko Kovačič <marko.kovacic@ibx.si>
2018-04-08 21:47:32 +0200

    transactions

---
+++ src/app/banking.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+import {Observable} from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
+@Injectable()
+export class BankingService {
+
+  private apiUrl = 'http://localhost:3000';  // URL to web api
+
+  constructor(private http: HttpClient) {
+  }
+
+  /**
+   * dobi vse transakcije
+   * @returns {Observable<any>}
+   */
+  getTransactions(): Observable<any> {
+    return this.http.get(this.apiUrl + '/transactions');
+  }
+
+  /**
+   * zapiše transakcijo z amountom
+   * @param data
+   * @returns {Observable<any>}
+   */
+  depositAmount(data): Observable<any> {
+    return this.http.post(this.apiUrl + '/transactions', data, httpOptions);
+  }
+
+}
